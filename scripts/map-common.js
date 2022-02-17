772703f16e5b51982c80a66d669677ee24746a67
LaoshuBaby <keaitianxinmail@qq.com>
2022-02-17 17:24:21 +0800

    Restructure folder

---
+++ scripts/map-common.js
@@ -0,0 +1,17 @@
+function parseMapHash() {
+  let matched = location.hash.match(/^#map=([.\d]+)\/([.\d]+)\/([.\d]+)/);
+  if (matched) {
+    return {
+      zoom: Number(matched[1]),
+      latlon: [ matched[2], matched[3] ]
+    }
+  }
+}
+
+function getMapHash(zoom, center) {
+  if (zoom < 17) {
+    return `#map=${zoom}/${center.lat.toFixed(4)}/${center.lng.toFixed(4)}`;
+  } else {
+    return `#map=${zoom}/${center.lat.toFixed(5)}/${center.lng.toFixed(5)}`;
+  }
+}
